ce72d94cf3883fa976fc7e34a413e602c155c5e5
arnaud sellenet <arnodmental@gmail.com>
2015-11-04 08:49:41 +0100

    install bootstrap with bower

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ Bundler.require(*Rails.groups)
 
 module Hashpedia
   class Application < Rails::Application
+    Proc.new { |path| path =~ /font-awesome\/fonts/ and File.extname(path).in?(['.otf', '.eot', '.svg', '.ttf', '.woff']) }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,11 +1,12 @@
-# Be sure to restart your server when you modify this file.
-
-# Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.root.join('vendor/assets/components').to_s.tap do |bower_path|
+Rails.application.config.sass.load_paths << bower_path
+Rails.application.config.assets.paths << bower_path
+end
+# Precompile Bootstrap fonts
+Rails.application.config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
+Rails.application.config.assets.precompile << %r(components-font-awesome/fonts/[\w-]+\.(?:eot|svg|ttf|woff)$)
+# Minimum Sass number precision required by bootstrap-sass
+::Sass::Script::Value::Number.precision = [8, ::Sass::Script::Value::Number.precision].max
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
